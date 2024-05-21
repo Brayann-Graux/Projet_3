6c60db5000ab69a5279580c8b03560f68d2473e1
Brayann GRAUX <pics.prod45@gmail.com>
2024-05-21 02:33:28 +0200

    ajout tu login index/css/js

---
+++ FrontEnd/assets/JavaScript/login.js
@@ -0,0 +1,64 @@
+// Récupération des éléments du formulaire de login
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const seConnecter = document.getElementById("seConnecter")
+
+
+seConnecter.addEventListener("click", login)
+// Stockage des données utilisateur lors du login
+async function login(event) {
+    event.preventDefault()
+    const valeurEmail = email.value
+    const valeurPassword = password.value
+
+    try {
+        const response = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify({ email: valeurEmail, password: valeurPassword })
+        });
+        const data = await response.json();
+
+        if (data.token) {
+            localStorage.setItem("token", data.token);
+            window.location.href = "../html/index-admin.html";
+            console.log("Connexion réussie");
+            console.log("Token trouvé :", localStorage.getItem("token"));
+        } else {
+            afficherMessage("L'adresse mail ou le mot de passe est incorrect")
+        }
+    } catch (error) {
+        afficherMessage("Une erreur est survenue")
+        console.error("Erreur lors de la connexion:", error);
+    }
+}
+
+
+// AJOUTER LOGOUT AVEC SUPPRESSION TOKEN  CHANGER LOGIN EN LOGOUT MENU QUAND CO
+// LOGOUT QUAND STOP PAGE
+
+
+// Message d'erreur mail ou mdp
+function afficherMessage(message) {
+    const erreurPrecedente = document.querySelector(".message-erreur")
+    if (erreurPrecedente) {
+        erreurPrecedente.remove()
+    }
+    const erreurMessage = document.createElement("p");
+    erreurMessage.classList.add("message-erreur")
+    erreurMessage.textContent = message;
+    erreurMessage.style.color = "red";
+    erreurMessage.style.marginBottom = "10px";
+    seConnecter.parentNode.insertBefore(erreurMessage, seConnecter);
+}
+
+// Mot de passe visible ou crypté
+const togglePassword = document.getElementById('toggle-password');
+const passwordField = document.getElementById('password');
+
+togglePassword.addEventListener('click', function () {
+    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', type);
+    togglePassword.classList.toggle('fa-eye-slash');
+    togglePassword.classList.toggle('fa-eye');
+});
